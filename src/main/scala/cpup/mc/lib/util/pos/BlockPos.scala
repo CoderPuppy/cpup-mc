994f7cdf6ecde2a9879ee0cfbdfb3b7b0a65a80d
CoderPuppy <cpup@cpup-laptop>
2014-03-18 20:20:40 -0400

    Add BlockPos

---
+++ src/main/scala/cpup/mc/lib/util/pos/BlockPos.scala
@@ -0,0 +1,25 @@
+package cpup.mc.lib.util.pos
+
+import net.minecraft.world.World
+import net.minecraft.block.Block
+import cpup.mc.lib.util.Direction
+
+case class BlockPos(world: World, x: Int, y: Int, z: Int) {
+	def setBlock(block: Block) = {
+		world.setBlock(x, y, z, block)
+
+		this
+	}
+
+	def setMetadata(meta: Int, notify: Int) = {
+		world.setBlockMetadataWithNotify(x, y, z, meta, notify)
+
+		this
+	}
+
+	def block = world.getBlock(x, y, z)
+	def metadata = world.getBlockMetadata(x, y, z)
+
+	def offset(offsetX: Int, offsetY: Int, offsetZ: Int) = BlockPos(x + offsetX, y + offsetY, z + offsetZ)
+	def offset(dir: Direction, dist: Int = 1) = offset(dir.forgeDir.offsetX * dist, dir.forgeDir.offsetY * dist, dir.forgeDir.offsetZ * dist)
+}
