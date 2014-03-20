df91210bca9abe5e2f281bf7cf1a6d752b5cd091
CoderPuppy <cpup@cpup-laptop>
2014-03-19 20:52:13 -0400

    Add stuff to CPupBlockContainer

--- src/main/scala/cpup/mc/lib/content/CPupBlockContainer.scala
+++ src/main/scala/cpup/mc/lib/content/CPupBlockContainer.scala
@@ -2,7 +2,19 @@ package cpup.mc.lib.content
 
 import cpup.mc.lib.{CPupModRef, CPupMod}
 import cpup.mc.lib.network.CPupMessage
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.world.World
+import net.minecraft.block.{ITileEntityProvider, Block}
 
-trait CPupBlockContainer[MOD <: CPupMod[_ <: CPupModRef, _ <: CPupMessage]] extends CPupBlock[MOD] {
+trait CPupBlockContainer[MOD <: CPupMod[_ <: CPupModRef, _ <: CPupMessage]] extends CPupBlock[MOD] with ITileEntityProvider {
+	override def breakBlock(world: World, x: Int, y: Int, z: Int, block: Block, meta: Int) {
+		super.breakBlock(world, x, y, z, block, meta)
+		world.removeTileEntity(x, y, z)
+	}
 
+	override def onBlockEventReceived(world: World, x: Int, y: Int, z: Int, id: Int, param: Int) = {
+		super.onBlockEventReceived(world, x, y, z, id, param)
+		val te = world.getTileEntity(x, y, z)
+		if(te != null) te.receiveClientEvent(id, param) else false
+	}
 }
