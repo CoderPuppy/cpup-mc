2ad93732bac6e9fbc062b70acf01172690307e83
CoderPuppy <cpup@cpup-laptop>
2014-04-04 08:21:49 -0400

    Add FMLServerStartingEvent to ModLifecycleHandler and let ModLifecycleHandler be registered with CPupMods

--- src/main/scala/cpup/mc/lib/CPupMod.scala
+++ src/main/scala/cpup/mc/lib/CPupMod.scala
@@ -3,7 +3,7 @@ package cpup.mc.lib
 import org.apache.logging.log4j.LogManager
 import cpup.mc.lib.content.CPupContent
 import cpw.mods.fml.common.Mod.EventHandler
-import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
+import cpw.mods.fml.common.event.{FMLServerStartingEvent, FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
 import cpup.mc.lib.network.{CPupMessage, CPupNetwork}
 
 trait CPupMod[REF <: CPupModRef] {
@@ -12,18 +12,35 @@ trait CPupMod[REF <: CPupModRef] {
 
 	final val logger = LogManager.getLogger(ref.modID)
 
+	protected var _lifecycleHandlers = List[ModLifecycleHandler]()
+	def registerLifecycleHandler(handler: ModLifecycleHandler) = {
+		_lifecycleHandlers = _lifecycleHandlers ++ List(handler)
+		this
+	}
+
 	@EventHandler
 	def preInit(e: FMLPreInitializationEvent) {
 		if(content != null) content.preInit(e)
+
+		_lifecycleHandlers.foreach(_.preInit(e))
 	}
 
 	@EventHandler
 	def init(e: FMLInitializationEvent) {
 		if(content != null) content.init(e)
+
+		_lifecycleHandlers.foreach(_.init(e))
 	}
 
 	@EventHandler
 	def postInit(e: FMLPostInitializationEvent) {
 		if(content != null) content.postInit(e)
+
+		_lifecycleHandlers.foreach(_.postInit(e))
+	}
+
+	@EventHandler
+	def serverStarting(e: FMLServerStartingEvent) {
+		_lifecycleHandlers.foreach(_.serverStarting(e))
 	}
 }
--- src/main/scala/cpup/mc/lib/ModLifecycleHandler.scala
+++ src/main/scala/cpup/mc/lib/ModLifecycleHandler.scala
@@ -1,9 +1,10 @@
 package cpup.mc.lib
 
-import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLPreInitializationEvent, FMLInitializationEvent}
+import cpw.mods.fml.common.event.{FMLServerStartingEvent, FMLPostInitializationEvent, FMLPreInitializationEvent, FMLInitializationEvent}
 
 trait ModLifecycleHandler {
 	def preInit(e: FMLPreInitializationEvent) {}
 	def init(e: FMLInitializationEvent) {}
 	def postInit(e: FMLPostInitializationEvent) {}
+	def serverStarting(e: FMLServerStartingEvent) {}
 }
