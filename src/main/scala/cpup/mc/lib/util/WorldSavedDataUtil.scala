ca10432760e523a202eddb77f6577f770d8b6fc4
CoderPuppy <cpup@cpup-laptop>
2014-04-04 17:24:11 -0400

    Uh, yeah. Derp. This is very neccesary

--- src/main/scala/cpup/mc/lib/util/WorldSavedDataUtil.scala
+++ src/main/scala/cpup/mc/lib/util/WorldSavedDataUtil.scala
@@ -1,7 +1,22 @@
 package cpup.mc.lib.util
 
 import net.minecraft.world.{World, WorldSavedData}
+import cpup.mc.lib.mod.CPupLib
 
 object WorldSavedDataUtil {
 	def get[DATA <: WorldSavedData](world: World, cla: Class[DATA], id: String) = world.loadItemData(cla, id).asInstanceOf[DATA]
+	def getOrCreate[DATA <: WorldSavedData](world: World, cla: Class[DATA], id: String) = {
+		var data = get(world, cla, id)
+		if(data == null) {
+			try {
+				data = cla.getConstructor(classOf[String]).newInstance(id)
+				world.setItemData(id, data)
+			} catch {
+				case e: Exception =>
+					CPupLib.logger.error(e)
+					CPupLib.logger.error(e.getStackTraceString)
+			}
+		}
+		data
+	}
 }
