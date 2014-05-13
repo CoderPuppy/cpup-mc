1bccf6f111d1ca28fbbdc73607c5e88c46046e51
CoderPuppy <cpup@cpup-laptop>
2014-05-12 23:13:24 -0400

    Make InvUtil.getItems return a list

--- src/main/scala/cpup/mc/lib/util/InvUtil.scala
+++ src/main/scala/cpup/mc/lib/util/InvUtil.scala
@@ -3,5 +3,5 @@ package cpup.mc.lib.util
 import net.minecraft.inventory.IInventory
 
 object InvUtil {
-	def getItems(inv: IInventory) = (0 to inv.getSizeInventory).map(inv.getStackInSlot(_)).toArray
+	def getItems(inv: IInventory) = (0 to inv.getSizeInventory).map(inv.getStackInSlot(_)).toList
 }
