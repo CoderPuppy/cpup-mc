de75acc1886527b0a67de12833b1a04449345de4
CoderPuppy <cpup@cpup-laptop>
2014-05-12 23:24:46 -0400

    Add logging for Modules

--- src/main/scala/cpup/mc/lib/intermod/baubles/BaublesI.scala
+++ src/main/scala/cpup/mc/lib/intermod/baubles/BaublesI.scala
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import baubles.api.BaublesApi
 import cpup.mc.lib.util.InvUtil
+import cpup.mc.lib.mod.CPupLib
 
 trait Interface extends ModLifecycleHandler {
 	def getItems(player: EntityPlayer): List[ItemStack]
@@ -19,4 +20,4 @@ object Disabled extends Interface {
 	def getItems(player: EntityPlayer) = List()
 }
 
-object BaublesI extends Module(ModLoadedCondition("Baubles"), Enabled, Disabled)
+object BaublesI extends Module(CPupLib, ModLoadedCondition("Baubles"), Enabled, Disabled)
--- src/main/scala/cpup/mc/lib/module/Module.scala
+++ src/main/scala/cpup/mc/lib/module/Module.scala
@@ -1,17 +1,23 @@
 package cpup.mc.lib.module
 
-import cpup.mc.lib.ModLifecycleHandler
+import cpup.mc.lib.{CPupModRef, CPupMod, ModLifecycleHandler}
 import cpw.mods.fml.common.event.{FMLServerStartingEvent, FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
 
-class Module[MOD <: ModLifecycleHandler](val condition: TModuleCondition, enabled: => MOD, disabled: => MOD) extends ModLifecycleHandler {
+class Module[MOD <: ModLifecycleHandler](val mod: CPupMod[_ <: CPupModRef], val condition: TModuleCondition, enabled: => MOD, disabled: => MOD) extends ModLifecycleHandler {
 	val impl = if(condition.canLoad) {
 		try {
-			enabled
+			mod.logger.info(s"[${getClass.getSimpleName}] Loading")
+			val res = enabled
+			mod.logger.info(s"[${getClass.getSimpleName}] Loaded")
+			res
 		} catch {
 			case e: Exception =>
+				mod.logger.info(s"[${getClass.getSimpleName}] Threw while loading")
+				e.printStackTrace
 				disabled
 		}
 	} else {
+		mod.logger.info(s"[${getClass.getSimpleName}] Condition not met, not loading")
 		disabled
 	}
 
