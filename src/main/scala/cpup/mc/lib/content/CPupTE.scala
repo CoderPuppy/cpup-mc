88437c5ba6e65118395d10fa4bdb93ef3bda0543
CoderPuppy <cpup@cpup-laptop>
2014-09-11 15:23:55 -0400

    Add the mod to CPupTE

--- src/main/scala/cpup/mc/lib/content/CPupTE.scala
+++ src/main/scala/cpup/mc/lib/content/CPupTE.scala
@@ -2,8 +2,10 @@ package cpup.mc.lib.content
 
 import net.minecraft.tileentity.TileEntity
 import cpup.mc.lib.util.pos.BlockPos
+import cpup.mc.lib.{CPupModRef, CPupMod}
 
-trait CPupTE extends TileEntity {
+trait CPupTE[MOD <: CPupMod[_ <: CPupModRef]] extends TileEntity {
+	def mod: MOD
 	def world = getWorldObj
 	def x = xCoord
 	def y = yCoord
