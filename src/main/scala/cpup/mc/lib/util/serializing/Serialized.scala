d6c4b63ec5425743f14765ce5718cd90d986b410
CoderPuppy <cpup@cpup-laptop>
2014-09-12 21:04:12 -0400

    This didn't really work (it used Nothing for T)

--- src/main/scala/cpup/mc/lib/util/serializing/Serialized.scala
+++ src/main/scala/cpup/mc/lib/util/serializing/Serialized.scala
@@ -4,6 +4,5 @@ import net.minecraft.nbt.NBTTagCompound
 
 object Serialized {
 	def apply(data: Any) = SerializationRegistry.writeToNBT(data)
-	def unapply[T](nbt: NBTTagCompound)(implicit manifest: Manifest[T]) = Option(SerializationRegistry.readFromNBT[T](nbt))
 	def un[T](nbt: NBTTagCompound)(implicit manifest: Manifest[T]) = SerializationRegistry.readFromNBT[T](nbt)
 }
