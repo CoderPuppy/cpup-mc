5c71d04ab06d28c9c8a69dc69065b9b27f3ecb83
CoderPuppy <cpup@cpup-laptop>
2014-10-07 21:27:24 -0400

    IDEA didn't like this

--- src/main/scala/cpup/mc/lib/util/InvUtil.scala
+++ src/main/scala/cpup/mc/lib/util/InvUtil.scala
@@ -3,5 +3,5 @@ package cpup.mc.lib.util
 import net.minecraft.inventory.IInventory
 
 object InvUtil {
-	def getItems(inv: IInventory) = (0 to inv.getSizeInventory).map(inv.getStackInSlot(_)).toList
+	def getItems(inv: IInventory) = (0 to inv.getSizeInventory).map(inv.getStackInSlot).toList
 }
